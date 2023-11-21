b5ea99a93f38673c33bc1814040199a5de815307
qrisma <nobel77.lee@gmail.com>
2023-11-21 14:50:23 +0900

    v1 : 기본적인 useReducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useReducer, useState } from "react";
+
+const ACTION_TYPES = {
+  deposit: "deposit",
+  withdraw: "withdraw",
+};
+
+const reducer = (state, action) => {
+  console.log("reducer가 일을 합니다.", state, action);
+
+  switch (action.type) {
+    case ACTION_TYPES.deposit:
+      return Number(state) + Number(action.payload);
+    case ACTION_TYPES.withdraw:
+      return Number(state) - Number(action.payload);
+    default:
+      return state;
+  }
+};
+
+// reducer - state를 업데이트하는 역할
+// dispatch - state 업데이트를 위한 인터페이스
+// action - 요구의 내용
 
 function App() {
+  console.log("Render");
+
+  const [number, setNumber] = useState(0);
+  // 잔고
+  const [myMoney, dispatch] = useReducer(reducer, 0);
+  // 입급
+  const onClickDeposit = () => {
+    console.log("onClickDeposit : dispatch 호출");
+    dispatch({ type: ACTION_TYPES.deposit, payload: number });
+  };
+  // 출금
+  const onClickWithdraw = () => {
+    console.log("onClickWithdraw: dispatch 호출");
+    dispatch({ type: ACTION_TYPES.withdraw, payload: number });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Welcome to useReducer Bank</h2>
+      <hr />
+      잔고 상태를 reducer를 이용하여 수정합니다.
+      <hr />
+      <p>잔고:{myMoney}원</p>
+      <input onChange={(e) => setNumber(e.target.value)} type="number" min="0" value={number} step="1000" />
+      <button onClick={onClickDeposit}>예금</button>
+      <button onClick={onClickWithdraw}>출금</button>
     </div>
   );
 }
